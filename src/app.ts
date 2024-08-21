3a0f8513bfbe064ee6aaac5123e9a6634b7f59b6
Proshanto08 <proshantosaha@6sensehq.com>
2024-08-21 12:58:22 +0600

    ratelimit middleware initialized

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import corsMiddleware from './middleware/corsMiddleware';
+import rateLimiter from './middleware/rateLimitMiddleware';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(corsMiddleware);
+app.use(rateLimiter);
 
 
 app.get('/', (req: Request, res: Response) => {
