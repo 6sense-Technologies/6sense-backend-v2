acb03ccd9c0fe25e332a262b0039834357eedf84
Proshanto08 <proshantosaha@6sensehq.com>
2024-08-21 13:04:20 +0600

    added hello-world route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import corsMiddleware from './middleware/corsMiddleware';
 import rateLimiter from './middleware/rateLimitMiddleware';
+import helloWorldRoutes from './routes/helloWorldRoutes';
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,5 +13,8 @@ app.use(rateLimiter);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello 6sense. And app is running on port 3000');
 });
+
+
+app.use('/api', helloWorldRoutes);
 
 export default app;
