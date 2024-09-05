a3a9d797c05b2cb2bfa5ce313d76cf7435d63f40
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-09-05 23:21:59 +0600

    solve codacy errors

--- src/utils/responseHandlers.ts
+++ src/utils/responseHandlers.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { IApiResponse } from "../types";
 
 export const handleSuccess = (
@@ -12,16 +13,27 @@ export const handleSuccess = (
 };
 
 export const handleError = (
-  error: any,
+  error: Error | AxiosError,
   errorCode?: string,
   message?: string,
   statusCode = 500,
 ): IApiResponse => {
-  const errorResponse = error?.response?.data || {};
+  let errorMessage = message || error.message || "An error occurred";
+  let errorStatusCode = statusCode;
+  let errorData = {};
+  let errorResponseCode: string | undefined = errorCode;
+
+  if (error instanceof AxiosError) {
+    errorMessage = message || error.response?.data?.message || error.message;
+    errorStatusCode = error.response?.status || statusCode;
+    errorResponseCode = errorCode || error.response?.data?.code;
+    errorData = error.response?.data || {};
+  }
+
   return {
-    status: statusCode,
-    errorCode: errorCode || errorResponse.code,
-    message: message || error.message || "An error occurred",
-    data: {},
+    status: errorStatusCode,
+    errorCode: errorResponseCode,
+    message: errorMessage,
+    data: errorData,
   };
 };
